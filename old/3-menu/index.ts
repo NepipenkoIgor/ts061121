8a4b62dd6a63771748688364ff1ba9056d8cf65e
Igor Nepipenko <inepipenko@muellerone.com>
2021-11-07 13:25:58 +0200

    feat(no-ref): test+ fn

---
+++ old/3-menu/index.ts
@@ -0,0 +1,32 @@
+import { generateMenu } from './menu';
+import { list } from './menu/data';
+
+// declare function $(selector: string): any;
+
+// declare const vk: any;
+
+declare const parseStackTrace: any;
+
+$('.menu')
+	.html(generateMenu(list))
+	.on('click', (e: Event) => {
+		const el: HTMLElement = e.target as HTMLElement;
+		if (!el.classList.contains('title')) {
+			return;
+		}
+		const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
+		parentLi.classList.toggle('menu-open');
+	});
+
+// const navMenu: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
+//
+// navMenu.innerHTML = generateMenu(list);
+//
+// navMenu.addEventListener('click', (e: MouseEvent) => {
+// 	const el: HTMLElement = e.target as HTMLElement;
+// 	if (!el.classList.contains('title')) {
+// 		return;
+// 	}
+// 	const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
+// 	parentLi.classList.toggle('menu-open');
+// });
